12a2d9c10fdb8a9ccb4f139d4b20e276f4927065
James2124 <poonkuanbin@gmail.com>
2025-02-05 10:48:39 +0800

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,21 @@
+<footer>
+        <div class="footer-bg py-5 py-md-4 px-5">
+                <div class="container-fluid">
+                        <div class="row justify-content-between align-items-center">
+                                <div class="col-12 col-md-3 pb-4 pb-md-0">
+                                        <div class="foot-logo mx-auto mx-md-0">
+                                                <img src="img/logo.svg">
+                                        </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-5">
+                                        <div class="copyright">
+                                                <p class="text-white text-center text-md-end">© 2020 Insert Name Here. All rights reserved.</p>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</footer>
---
+++ index.php
@@ -0,0 +1,236 @@
+<?php
+$page_title = 'Home';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include('head.php'); ?>
+
+<body>
+    <main class="bg-white">
+        <section class="section1 section1-bg">
+            <div class="container-fluid container-width contain-center">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12">
+                        <div class="logo">
+                            <img class="logo-img" src="img/logo.svg">
+                        </div>
+                    </div>
+                    <div class="col-12 py-4 py-xxl-5">
+                        <div class="section1-title">
+                            <h1 class="text-mont">Concisely <span class="d-block">describe your</span><span class="d-block">product or service</span></h1>
+                        </div>
+                    </div>
+                    <div class="col-12 pb-4 pb-xxl-5">
+                        <div class="section1-content">
+                            <p class="font-16 text-roboto text-white">No need to get clever. Tell people exactly what you're offering, then <span class="d-md-block">use this space to communicate your key value proposition.</span></p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="btn-wrap text-center">
+                            <a href="#" class="">ORDER NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section2">
+            <div class="container-fluid container-width">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12 col-md-10">
+                        <div class="section2-title text-center">
+                            <h3 class="text-mont">Here are some of the benefits of your offer</h3>
+                        </div>
+                    </div>
+                    <div class="col-12 py-4 py-xxl-5">
+                        <div class="section2-content text-center mx-auto">
+                            <p class="text-mont font-20">Explain what makes your product or service great. Talk about features in a way that highlights the real value people get out of them.</p>
+                        </div>
+                    </div>
+                    <div class="col-12 pt-0 pt-md-5">
+                        <div class="container-fluid">
+                            <div class="d-flex flex-column flex-md-row justify-content-center align-items-stretch">
+                                <div class="col-12 col-md-4 pt-4 pb-2 py-md-0">
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <div class="col-12">
+                                            <div class="section2-item-wrap text-center">
+                                                <div class="items-img items-1">
+                                                    <img src="img/section1-item1.png">
+                                                </div>
+                                                <div class="item-title pt-4 pb-3">
+                                                    <p class="fw-bold font-20 text-mont">Benefit 1</p>
+                                                </div>
+                                                <div class="item-content">
+                                                    <p class=" font-16 text-pt">For example, restaurants and bakeries could outline the health benefits of their all-natural ingredients.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4 py-2 py-md-0">
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <div class="col-12">
+                                            <div class="section2-item-wrap text-center">
+                                                <div class="items-img items-2">
+                                                    <img src="img/section1-item2.png">
+                                                </div>
+                                                <div class="item-title pt-4 pb-3">
+                                                    <p class="fw-bold font-20 text-mont">Benefit 2</p>
+                                                </div>
+                                                <div class="item-content">
+                                                    <p class="font-16 text-pt">Florists and other small retailers might use this space to describe how their products make delightful, one-of-a-kind gifts.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4 py-2 py-md-0">
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <div class="col-12">
+                                            <div class="section2-item-wrap text-center">
+                                                <div class="items-img items-3">
+                                                    <img src="img/section1-item3.png">
+                                                </div>
+                                                <div class="item-title pt-4 pb-3">
+                                                    <p class="fw-bold font-20 text-mont">Benefit 3</p>
+                                                </div>
+                                                <div class="item-content">
+                                                    <p class="font-16 text-pt">You could also add buttons to this section and share links to other products or available delivery methods.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section3 section3-bg">
+            <div class="container-fluid container-width">
+                <div class="row justify-content-center align-items-center text-center">
+                    <div class="col-12 pb-4 pb-xxl-5">
+                        <div class="section3-title">
+                            <p class="font-20 text-mont text-white fw-semibold">What our customers say</p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="quata-mark">
+                            <p class="text-pt">“</p>
+                        </div>
+                    </div>
+                    <div class="col-12 pb-4 pb-xxl-5">
+                        <div class="section3-content">
+                            <h4 class="text-pt text-white">Share a real testimonial that hits some of your benefits (but isn’t too sales-y).</h4>
+                        </div>
+                    </div>
+                    <div class="col-12 pt-4 pt-md-5">
+                        <div class="section3-user pb-3">
+                            <img src="img/customer1.png">
+                        </div>
+                        <div class="section3-detail">
+                            <p class="text-white section3-username">REAL NAME</p>
+                            <p class="text-white section3-location">location</p>
+                            <div class="section3-star">
+                                <img src="img/customer-star.svg">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section4">
+            <div class="container-fluid container-width">
+                <div class="row justify-content-center align-items-stretch">
+                    <div class="col-12 col-md-4 text-center">
+                        <div class="section4-feature pb-5 pb-md-0">
+                            <div class="feature-img pb-4 pb-xxl-5">
+                                <img src="img/feature1.jpg">
+                            </div>
+                            <div class="section4-title">
+                                <p class="fw-bold font-20 text-mont">Feature 1</p>
+                            </div>
+                            <div class="section4-content">
+                                <p class="font-16 text-pt">Talk about some of the details of your offer with a focus on the value people get back.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 text-center">
+                        <div class="section4-feature pb-5 pb-md-0">
+                            <div class="feature-img pb-4 pb-xxl-5">
+                                <img src="img/feature2.jpg">
+                            </div>
+                            <div class="section4-title">
+                                <p class="fw-bold font-20 text-mont">Feature 2</p>
+                            </div>
+                            <div class="section4-content">
+                                <p class="font-16 text-pt">Is there a pain point that your product or service resolves? Tell visitors about it here.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 text-center">
+                        <div class="section4-feature">
+                            <div class="feature-img pb-4 pb-xxl-5">
+                                <img src="img/feature3.jpeg">
+                            </div>
+                            <div class="section4-title">
+                                <p class="fw-bold font-20 text-mont">Feature 3</p>
+                            </div>
+                            <div class="section4-content">
+                                <p class="font-16 text-pt">Alternatively, you could use this section to address some frequently asked questions.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section5">
+            <div class="container-fluid container-width">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-12 col-md-6 col-lg-5">
+                        <div class="section5-img d-none d-md-block">
+                            <img src="img/information.jpg">
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <div class="section5-detail">
+                            <div class="d-flex flex-column justify-content-center align-items-center gap-5">
+                                <div class="section5-title">
+                                    <h3 class="text-mont">Some more information about your business</h3>
+                                </div>
+                                <div class="section5-img d-block d-md-none">
+                                    <img src="img/information.jpg">
+                                </div>
+                                <div class="section5-content">
+                                    <p class="font-16 text-pt">Share a little about yourself as a business owner, or maybe describe what makes your product or service unique. Give visitors one more reason to care about your offer and want to buy from you.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section6 section6-bg text-center mx-auto">
+            <div class="container-fluid container-width">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12 ">
+                        <div class="section6-title">
+                            <h2 class="text-mont">End with one <span class="d-lg-block">final call to action</span></h2>
+                        </div>
+                    </div>
+                    <div class="col-12 py-4 py-xxl-5">
+                        <div class="section6-content">
+                            <p class="font-16 text-pt">This is your last shot at converting someone. Remind them about the <span class="d-lg-block">benefits of your offer and encourage them to act now.</span></p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="btn-wrap text-center mx-auto">
+                            <a href="#" class="">ORDER NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include 'foot.php'; ?>
+</body>
